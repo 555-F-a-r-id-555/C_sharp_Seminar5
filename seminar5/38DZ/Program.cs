ae43be4c90d5740ee5c2147b62c74005315133d0
Farid Bagirov <makflay85zet@gmail.com>
2023-02-26 01:04:32 +0400

    changed 38DZ

--- seminar5/38DZ/Program.cs
+++ seminar5/38DZ/Program.cs
@@ -3,25 +3,27 @@
 // минимальным элементов массива.
 // [3 7 22 2 78] -> 76
 
-int[] randomArray(int length)
+Double[] randomArray(int length)
 {
-    int[] a = new int[length];
-    for (int i = 0; i < length; i++) a[i] = Random.Shared.Next(100, 1000);
+    double[] a = new double[length];
+    for (int i = 0; i < length; i++) a[i] = Math.Round(Random.Shared.NextDouble() * 100, 2);
     return a;
 }
 
-void res(int[] a)
+// Console.WriteLine($"{Random.Shared.NextDouble()*100}");
+
+void res(double[] a)
 {
-    int min = a[0];
-    int max = a[0];
+    var min = a[0];
+    var max = a[0];
     foreach (var item in a)
     {
         if (item < min) min = item;
         if (item > max) max = item;
     }
-    Console.WriteLine($"[{string.Join(", ",a)}] -> {max} - {min} = {max-min}");
-    
+    Console.WriteLine($"[{string.Join(", ", a)}] -> {max} - {min} = {(max - min):f2}\n\n");
 }
+
 
 res(randomArray(5));
 res(randomArray(5));
